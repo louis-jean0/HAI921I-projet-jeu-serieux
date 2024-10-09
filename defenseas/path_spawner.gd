e810567be62a7d411c0f29d96407ebf968e030cf
bserva34 <benjamin.serva34@gmail.com>
2024-10-09 10:19:10 +0200

    ajout d'un spawn supp

--- defenseas/path_spawner.gd
+++ defenseas/path_spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var path = preload("res://Stage_1.tscn")
 @onready var path2 = preload("res://Stage_2.tscn")
+@onready var path3 = preload("res://Stage_3.tscn")
 
 
 func _on_timer_timeout() -> void:
@@ -10,3 +11,5 @@ func _on_timer_timeout() -> void:
 	add_child(tempPath)
 	var tempPath2 = path2.instantiate();
 	add_child(tempPath2)
+	var tempPath3 = path3.instantiate();
+	add_child(tempPath3)
