ac15aa99c5e3411f827f8d477b32f22346649b42
bserva34 <benjamin.serva34@gmail.com>
2024-10-13 17:14:01 +0200

    menu tour cook

--- defenseas/Towers/tower_1_panel.gd
+++ defenseas/Towers/tower_1_panel.gd
@@ -1,16 +1,23 @@
 extends Panel
 
-@onready var tower = preload("res://Towers/pince.tscn")
-var currTile
+@onready var tower = preload("res://Towers/first_tower.tscn")
+var current_tower = null  # Variable pour stocker la tour en cours de placement
 
 func _on_gui_input(event):
-	var tempTower = tower.instantiate()
+	# Vérifier si l'utilisateur appuie sur le bouton gauche de la souris
 	if event is InputEventMouseButton and event.button_mask == 1:
-		#left down
-		add_child(tempTower)
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-	elif event is InputEventMouseButton and event.button_mask == 1:	
-		#lefi up
-		get_child(1).global_position = event.global_position
-	elif event is InputEventMouseButton and event.button_mask == 0:
-		print("Left up")
+		if event.is_pressed():
+			# Bouton gauche enfoncé, créer une tour
+			current_tower = tower.instantiate()
+			add_child(current_tower)
+			current_tower.global_position = event.global_position
+			current_tower.process_mode = Node.PROCESS_MODE_DISABLED  # Désactiver le processus temporairement
+		else:
+			# Bouton gauche relâché, arrêter de suivre la souris et activer la tour
+			if current_tower:
+				current_tower.process_mode = Node.PROCESS_MODE_INHERIT  # Réactiver le processus
+				current_tower = null  # Réinitialiser la tour actuelle
+				
+	# Si la tour est en cours de placement, la faire suivre la souris
+	if current_tower and event is InputEventMouseMotion:
+		current_tower.global_position = event.global_position
