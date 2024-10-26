00c18d74291267aad24cb6d4372d250d1583b362
Loick01 <loicker89@gmail.com>
2024-10-26 11:23:48 +0000

    Prise des déchets par la pince + il tombe et disparaisse quand on les relache

--- Defenseas_Crush/player_boat.gd
+++ Defenseas_Crush/player_boat.gd
@@ -10,15 +10,23 @@ var pinceY = 0
 var boatLook = 1 # 1 pour left, -1 pour right (permet de bien positionner la pince)
 var scaleX
 var scaleY
+var holdingWaste # Sprite du déchet porté par la pince
+var isHoldingWaste = false
+var fallingWaste = []
+var fallingSpeed = 3
+var window_size
 
 var stringColor = Color(55,55,55)
-
+var texture_paths = [preload("res://waste1.png"), preload("res://waste2.png"), preload("res://waste3.png")]
 var wasteGrid
 
-var holdingWaste = false
+var frameIndex
+var spriteFrames
+var grabTexture
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	window_size = get_viewport_rect().size # Récupère les dimensions de la fenêtre
 	boat = self.get_child(0)
 	pince = boat.get_child(0)
 	boat.animation="goLeft"
@@ -29,16 +37,16 @@ func _ready() -> void:
 	wasteGrid = get_parent().get_node("wasteGrid")
 	scaleX = pince.scale.x
 	scaleY = pince.scale.y
+	frameIndex = pince.get_frame()
+	spriteFrames = pince.get_sprite_frames()
+	grabTexture = spriteFrames.get_frame_texture("closeGrap", frameIndex)
 
 func _draw():
 	# Dessine le fil reliant le bateau à la pince
-	var frameIndex: int = pince.get_frame()
-	var spriteFrames: SpriteFrames = pince.get_sprite_frames()
-	var currentTexture: Texture2D = spriteFrames.get_frame_texture("closeGrap", frameIndex)
-	draw_line(Vector2(boat.position.x + pince.position.x + currentTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y - 50), Vector2(boat.position.x + pince.position.x + currentTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y + pince.position.y*2), Color.DARK_SLATE_GRAY, 10.0)
+	draw_line(Vector2(boat.position.x + pince.position.x + grabTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y - 50), Vector2(boat.position.x + pince.position.x + grabTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y + pince.position.y*2), Color.DARK_SLATE_GRAY, 10.0)
 
 	# Dessine un point là où vise la pince
-	# draw_circle(Vector2i(boat.position.x + pince.position.x + currentTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y + pince.position.y*2 + currentTexture.get_size().y*scaleY), 9.3905, Color.DARK_RED)
+	# draw_circle(Vector2i(boat.position.x + pince.position.x + grabTexture.get_size().x * scaleX/1.5 * boatLook, boat.position.y + pince.position.y*2 + grabTexture.get_size().y*scaleY), 9.3905, Color.DARK_RED)
 	
 func _process(delta):	
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -48,14 +56,25 @@ func _process(delta):
 		velocity.x -= 1
 	if Input.is_action_just_pressed("grabAction"):
 		pince.animation = "closeGrap"
-		
-		var frameIndex: int = pince.get_frame()
-		var spriteFrames: SpriteFrames = pince.get_sprite_frames()
-		var currentTexture: Texture2D = spriteFrames.get_frame_texture("closeGrap", frameIndex)
-		var numWaste = wasteGrid.checkAt(boat.position.x + pince.position.x + currentTexture.get_size().x * scaleX/1.5 * boatLook + 10, boat.position.y + pince.position.y*2 + currentTexture.get_size().y*scaleY)
-		
+	
+		var typeWaste = wasteGrid.checkAt(boat.position.x + pince.position.x + grabTexture.get_size().x * scaleX/1.5 * boatLook + 10, boat.position.y + pince.position.y*2 + grabTexture.get_size().y*scaleY)
+		if typeWaste != -1:
+			isHoldingWaste = true
+			holdingWaste = Sprite2D.new()
+			holdingWaste.texture = texture_paths[typeWaste]
+			self.add_child(holdingWaste) # Ajoute le Sprite2D à la scène (sinon il n'est pas dessiné)
+			
 	if Input.is_action_just_released("grabAction"):
 		pince.animation = "openGrap"
+		if isHoldingWaste:
+			isHoldingWaste = false
+			# Réinitialise le sprite porté par la pince
+			var fw = holdingWaste.duplicate()
+			self.add_child(fw)
+			fallingWaste.append(fw)
+			holdingWaste.queue_free() # Supprime le noeud de la scène
+			holdingWaste = null
+		
 	if Input.is_action_pressed("downPince"):
 		if pinceY < 350:
 			pinceY += 5
@@ -68,6 +87,18 @@ func _process(delta):
 	
 	pince.position = Vector2i(50*boatLook,pinceY)
 	
+	if isHoldingWaste:
+		holdingWaste.position = Vector2i(boat.position.x + pince.position.x + grabTexture.get_size().x * scaleX/1.5 * boatLook + 10, boat.position.y + pince.position.y*2 + grabTexture.get_size().y*scaleY)
+	
+	for i in range(fallingWaste.size()):
+		# Si besoin de vérifier que fallingWaste[i] != null, faire --> if fallingWaste[i]
+		fallingWaste[i].position.y += fallingSpeed
+		if fallingWaste[i].position.y > window_size.y + 64:
+			fallingWaste[i].queue_free() # Supprime le noeud de la scène
+			fallingWaste[i] = null
+			fallingWaste.remove_at(i)
+			break # On est obligé de stopper la boucle for car les indices des éléments changent à cause du remove_at
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		if velocity.x > 0:
--- Defenseas_Crush/waste_grid.gd
+++ Defenseas_Crush/waste_grid.gd
@@ -8,13 +8,17 @@ extends GridContainer
 @export var cell_size = Vector2i(64, 64)
 @export var gridPosition = Vector2i(340,724)
 
-var indWaste = 0
+var indWaste
 
 var texture_paths = [preload("res://waste1.png"), preload("res://waste2.png"), preload("res://waste3.png")]
+var listTypeWaste = [] # Permettra de connaitre les objets manipulé par la pince
 
 func _ready():
 	clear_grid()
 	position = gridPosition
+	indWaste = 0
+	for i in range(grid_size.x*grid_size.y):
+		listTypeWaste.append(-1)
 	
 	var timer = Timer.new()
 	timer.wait_time = delay_between_spawn
@@ -31,7 +35,9 @@ func clear_grid():
 func spawn_new_object():
 	if indWaste < grid_size.x * grid_size.y:
 		var newWaste = Sprite2D.new()
-		newWaste.texture = texture_paths[randi() % texture_paths.size()]
+		var type = randi() % texture_paths.size()
+		listTypeWaste[indWaste] = type
+		newWaste.texture = texture_paths[type]
 		newWaste.position = Vector2i(indWaste % grid_size.y * cell_size.x, 600 - (indWaste / grid_size.y * cell_size.y))
 		indWaste+=1
 		# Ajouter le sprite à la grille
@@ -43,5 +49,10 @@ func checkAt(aimX,aimY):
 			if child.position.x + gridPosition.x < aimX and aimX < child.position.x + gridPosition.x + cell_size.x: # Bien prendre en compte les coordonnées monde des cellules (et pas par rapport à la grille)
 				if child.position.y + gridPosition.y < aimY and aimY < child.position.y + gridPosition.y + cell_size.y   :
 					self.remove_child(child)
+					var i = int((aimX-gridPosition.x)/cell_size.x) 
+					var j = int(((600+cell_size.y)-(aimY-gridPosition.y))/cell_size.y)	
+					var typeWaste = listTypeWaste[j*grid_size.y + i]
+					listTypeWaste[j*grid_size.y + i] = -1
+					return typeWaste	
 	return -1
 	
